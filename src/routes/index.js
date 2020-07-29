ece5100b705c71d2d29baecac5ac17ce6058d97d
Chemi Martinez <jmmartineztuarez@gmail.com>
2020-07-29 12:07:13 -0400

    CRUD completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,43 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('index')
+const Task = require('../models/task')
+
+router.get('/', async (req, res) => {
+    const Tasks = await Task.find()
+    res.render('index', {Tasks})
+})
+
+router.get('/turn/:id', async (req, res) => {
+    const { id } = req.params
+    const task = await Task.findById(id)
+    task.status = !task.status
+    await task.save()
+    res.redirect('/')
+})
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params
+    const task = await Task.findById(id)
+    res.render('edit', { task })
+})
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params
+    await Task.remove({_id: id})
+    res.redirect('/')
+})
+
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body)
+    await task.save()
+    res.redirect('/')
+})
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params
+    await Task.update({_id : id}, req.body)
+    res.redirect('/')
 })
 
 module.exports = router;
